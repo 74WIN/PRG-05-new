e777b4691537540cdc69ca6a351ed0ba17f11701
74win <ccw.la@outlook.com>
2021-09-23 15:39:33 +0200

    Ik heb de blade en routes verder aangepast

--- storyline/app/Http/Controllers/ElementContoller.php
+++ storyline/app/Http/Controllers/ElementContoller.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ElementContoller extends Controller
 {
+    public function index(){
+        return view('element');
+    }
     //
 }
--- storyline/resources/views/user.blade.php
+++ storyline/resources/views/user.blade.php
@@ -81,7 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Story
+            User
         </div>
 
         <div class="links">
