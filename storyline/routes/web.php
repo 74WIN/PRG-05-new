a231ff9052b91e6cf01c46492ae2d35328c0aeeb
74win <ccw.la@outlook.com>
2021-09-27 14:40:26 +0200

    Ik heb de 4 regels ingevoegd. Hierdoor zijn er paar bestanden toegevoegd.

--- storyline/routes/web.php
+++ storyline/routes/web.php
@@ -15,8 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::get('/about',[\App\Http\Controllers\AboutController::class, 'index']);
 Route::get('/weapon',[\App\Http\Controllers\WeaponsController::class, 'index']);
 Route::get('/element',[\App\Http\Controllers\ElementContoller::class, 'index']);
 Route::get('/user',[\App\Http\Controllers\UserController::class, 'index']);
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
